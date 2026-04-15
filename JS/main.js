7ea1e4948563142908c39d32874fb293da032a19
JuanJo Rivero <riverocamargojuanjose@gmail.com>
2026-04-15 11:51:05 -0500

    Se añade contenido

--- JS/main.js
+++ JS/main.js
@@ -68,17 +68,25 @@ const projectsData = [
     id: 1,
     title: "Fakestore",
     subtitle: "Modern E-commerce",
-    desc: "E-commerce platform with responsive design and advanced features including filtering, shopping cart, and product management, plus design features such as an activatable dark mode.",
+    desc: "A platform with responsive design and advanced features including filtering, shopping cart, and product management, plus design features such as an activatable dark mode.",
     github: "https://github.com/JuanJo-R-1/Fakestore-Proyecto.git",
     images: ["../Resources/Fakestore0.png", "../Resources/Fakestore1.png", "../Resources/Fakestore2.png", "../Resources/Fakestore3.png", "../Resources/Fakestore4.png"]
   },
   {
     id: 2,
     title: "Game-Test",
-    subtitle: "Simple Minigame entertaiment",
+    subtitle: "Simple Minigame for entertaiment",
     desc: "Classic minigame coleccion using a simple dark design with JavaScript logic.",
     github: "https://github.com/JuanJo-R-1/Web-Games-Project-JR",
     images: ["../Resources/tetris.png", "../Resources/serpiente.png", "../Resources/buscaminas.png", "../Resources/pacman.png"]
+  },
+  {
+    id: 3,
+    title: "Bomboloni",
+    subtitle: "E-commerce with a lot of variety",
+    desc: "It has a wide variety of options, payment gateway, interactive menu, real-time order processing, along with customer notifications for shipping. This was made with other people.",
+    github: "https://bomboloni-specialty-coffee-donuts.wuaze.com/catalogo?i=1",
+    images: ["../Resources/Bom.png", "../Resources/BomB.png", "../Resources/Bo", "../Resources/Bombo.png"]
   }
 ];
 
